23b6f32c8bb0fc2e99aa71b04fbd1f00bd41a5f9
tom_glb <goodmorning.glb@atguigu.com>
2020-02-08 23:53:18 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/hello.java
@@ -0,0 +1,20 @@
+package com.atguigu.bean;
+
+/**
+ * @author jia
+ * @date 2020/2/8-20:08
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int num=10;
+        System.out.println("num = " + num);
+        System.out.println("args = [" + args + "]");
+        System.out.println("hello.main");
+        System.out.printf("");
+        String[] s=new String[]{"tom","jia","iii"};
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        private String name;
+        private static final int INITAL_OI=10;
+        Arr;
+        System.out.println("helloworld!");
+        ArrayList al=new ArrayList();
+        al.add();
+        Da te date = new Date();
+
+    }
+}
